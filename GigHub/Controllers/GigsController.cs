c6aacde45d0bb8169a6761aa6354f0af64a6f6d1
vlad94md <i22vladg@mail.ru>
2016-07-02 19:47:05 +0300

    Create a Gig form

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            GigFormViewModel model = new GigFormViewModel()
+            {
+                Genres = _context.Genres
+            };
+            return View(model);
+        }
+    }
+}
