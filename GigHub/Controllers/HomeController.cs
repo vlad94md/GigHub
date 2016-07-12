b672b7d7beb4b45976687ea29e00fc9d8ae9b5ea
vlad94md <i22vladg@mail.ru>
2016-07-12 14:15:50 +0300

    Build Attendance API

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -16,7 +16,10 @@ namespace GigHub.Controllers
 
         public ActionResult Index()
         {
-            var upcomingGigs = _context.Gigs.Include("Artist").Where(g => g.DateTime > DateTime.Now);
+            var upcomingGigs = _context.Gigs
+                .Include("Artist")
+                .Include("Genre")
+                .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
         }
